2115f897c0af543e607ee9811d2ae2e3b7a7a737
Hồng Duy <hongduy2601@gmail.com>
2025-11-29 23:30:28 +0700

    Initial commit - AIWS Dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import AIWSDashboard from './AIWSDashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AIWSDashboard />
   </React.StrictMode>,
 )
